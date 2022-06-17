4f5b8b16bf46c8ec548ed85793fe68a6afa6c613
Jerzy Slomkowski <jerzy.slomkowski@gmail.com>
2022-06-17 09:56:38 +0200

    [develop] split

---
+++ nofluf_analysis.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import json
+
+# df = pd.read_excel('data/result.xlsx')
+df = pd.read_csv('data/result.csv')
+
+df['value'] = df['value'].str.lower()
+
+# map inpropper values to propper
+with open('data/name_mapping.json', 'r') as f:
+    name_mapping = json.load(f)
+df['value'] = df['value'].replace(name_mapping)
+
+# split data into two groups: structured and unstructured
+move = [
+    'specs', 'benfs', 'primary_req', 'envs', 'secondary_req', 'gear',
+    'locations', 'tasks']
+unstructured_perks = df[df['variable'].isin(move)]
+structured_perks = df[~df['variable'].isin(move)]
+structured_perks = structured_perks.pivot(
+    index='key', columns='variable', values='value').reset_index()
+
+# change datatypes to int
+dtypes_change = ['B2B_cash_high', 'B2B_cash_low', 'UZ_cash_high', 'UZ_cash_low',
+                 'UoP_cash_high', 'UoP_cash_low']
+for c in dtypes_change:
+    structured_perks[c] = structured_perks[c].astype(pd.Int32Dtype())
+
+# change order of columns
+structured_perks = structured_perks[[
+    'timestamp', 'key', 'title', 'company', 'level_high', 'level_low',
+    'posting_time', 'remote', 'B2B_cash_currency', 'B2B_cash_high',
+    'B2B_cash_low', 'UZ_cash_currency', 'UZ_cash_high', 'UZ_cash_low',
+    'UoP_cash_currency', 'UoP_cash_high', 'UoP_cash_low', 'link'
+]]
+
+# save
+with pd.ExcelWriter('data/result.xlsx') as writer:
+    structured_perks.to_excel(
+        writer, sheet_name='structured_perks', index=False)
+    unstructured_perks.to_excel(
+        writer, sheet_name='unstructured_perks', index=False)
