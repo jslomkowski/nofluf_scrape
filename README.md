d6bf5ac2f48c5c10860336d9789aea62c3a2f60f
Jerzy Slomkowski <jerzy.slomkowski@gmail.com>
2022-12-23 07:06:44 +0100

    [develop] update

--- README.md
+++ README.md
@@ -6,4 +6,7 @@ noflufjobs scraper
 - [] fix 'tasks' functionality. They are empty now.
 
 ## usage:
-go to config.py and set the FIRST_PAGE as first page of category you want to scrape. Set NAME to the name of the category. Run nofluf_list_scrape.py to get the list of all jobs in that category. Run nofluf_job_scrape.py to get the details of each job.
+go to config.py and set the FIRST_PAGE as first page of category you want to
+scrape. Set NAME to the name of the category. Run nofluf_list_scrape.py to get
+the list of all jobs in that category. Run nofluf_job_scrape.py to get the
+details of each job.
--- config.py
+++ config.py
@@ -1,10 +1,13 @@
+import datetime
 
 # depends on what you want to scrap. Setup this as first page
 FIRST_PAGE = """
-https://nofluffjobs.com/pl/backend?page=1
+https://nofluffjobs.com/pl/artificial-intelligence?page=1
 """
 
-NAME = "be"
+### get date in format yyyy-mm-dd
+DATE = datetime.datetime.now().strftime("%Y-%m-%d")
+NAME = f"ai_{DATE}"
 
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36", "Accept-Encoding": "gzip, deflate",
            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT": "1", "Connection": "close", "Upgrade-Insecure-Requests": "1"}
