b683981ae7df7d89952ab23cbfe7948747b61167
Jerzy Slomkowski <jerzy.slomkowski@gmail.com>
2023-02-10 21:21:04 +0100

    [develop]

--- nofluf_analyze.py
+++ nofluf_analyze.py
@@ -1,30 +1,19 @@
 
 import pandas as pd
 import itertools
-import numpy as np
-df = pd.read_excel("data/results/2023-02-09_ai_output.xlsx")
 
-df_lite = df[["link", "primary_skils"]]
+df = pd.read_excel(r"data\results\2023-02-10_back_end\2023-02-10_back_end_output.xlsx")
 
-cols_lst = list(df_lite["primary_skils"].str.split(","))
-cols_lst = list(itertools.chain.from_iterable(cols_lst))
+df_cols = list(df["primary_skils"].str.split(", "))
+df_cols = list(itertools.chain.from_iterable(df_cols))
+df_cols = list(set(df_cols))
+df_cols = pd.DataFrame(columns=df_cols)
+df_split = df["primary_skils"].str.split(", ", expand=True)
 
-# cols_lst = [x.lower() for x in cols_lst]
-cols_lst = [x.strip() for x in cols_lst]
-cols_lst = list(set(cols_lst))
-
-df_cols_list = pd.DataFrame(columns=cols_lst)
-
-df_lite_expanded = pd.concat(
-    [df_lite['link'],
-     df_lite["primary_skils"].str.split(",", expand=True)], axis=1)
-
-df_lite_expanded = df_lite["primary_skils"].str.split(",", expand=True)
-
-for i in range(len(df_lite_expanded)):
+for i in range(len(df_split)):
     print(i)
-    i=0
-    df_lite_ex = pd.DataFrame(df_lite_expanded.iloc[i]).T
-    df_lite_ex.columns = df_lite_ex.values.tolist()[0]
-    df_cols_list = df_cols_list.append(df_lite_ex)
-    # ! TODO fix this
+    i = 100
+    _df = pd.DataFrame(df_split.iloc[i]).T
+    _df.dropna(axis=1, how="all", inplace=True)
+    _df.columns = _df.values.tolist()[0]
+    df_cols = pd.concat([df_cols, _df], axis=0)
